49c909cd8eedf589f3a478b46297f86ba78f9600
Patrick Huang <patrickhuang94@gmail.com>
2016-02-11 10:22:00 +0000

    New posts kind of work

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ var settings = require('./routes/settings');
 var userprofile = require('./routes/userprofile');
 var password = require('./routes/password');
 var aboutus = require('./routes/aboutus');
-var newindex = require('./routes/newindex');
 
 var app = express();
 
@@ -58,7 +57,6 @@ app.get('/settings', settings.view);
 app.get('/userprofile', userprofile.view);
 app.get('/password', password.view);
 app.get('/aboutus', aboutus.view);
-app.get('/newindex', newindex.view);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,12 @@ var data = require('../data.json');
 exports.view = function(req, res){
 	//console.log(data);
 	res.render('index', data);
+
+	var newPost = {
+		"title": "My name is Bob",
+		"description": "I make burgers everyday",
+		"imageURL": "http://lorempixel.com/400/400/people"
+	}
+
+	data["posts"].push(newPost);
 };
