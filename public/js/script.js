c96515d2a51c9f82b7bd5b4246047182c0bbb5d7
Patrick Huang <patrickhuang94@gmail.com>
2016-02-11 08:06:49 +0000

    Added error checking for password

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	$('#password-updated').hide();
+	$('#password-error').hide();
+
 	//Take user to user profile link
 	$('#user-profile').click(function() {
 		window.open("userprofile.html");
@@ -15,4 +18,21 @@ $(document).ready(function() {
 			text == "Save" ? "Edit" : "Save"
 		);
 	});
+
+	//Save button should prop up a confirmation when user saves new password
+	//Do some error checking for the user textfields
+	$('#password-button').click(function() {
+		var current = $('#current-password');
+		var newPass = $('#new-password');
+		var confirm = $('#confirm-password');
+
+		if (current.val().length == 0 || newPass.val().length == 0 || confirm.val().length == 0) {
+			$('#password-error').show();
+		}
+
+		else {
+			$('#password-updated').show();
+			$('#password-error').hide();
+		}
+	})
 });
