a2b51cbcf45924c666083bb5b3b52fa100ecce70
Patrick Huang <patrickhuang94@gmail.com>
2016-02-11 04:50:27 +0000

    Added password section

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
 	//Take user to user profile link
 	$('#user-profile').click(function() {
-		window.open("userprofile.html")
+		window.open("userprofile.html");
 	});
 
-	$(".clickable-row").click(function() {
+	$('.clickable-row').click(function() {
         window.document.location = $(this).data("href");
     });
 
-	//Needs to check if user credentials are verified -- haven't done so here
-    /*$("#login-button").click(function() {
-    	window.location("../../views/index.handlebars");
-    });*/
-}
+	$('#profile-button').click(function() {
+		var text = $('#profile-button').text();
+		$('#profile-button').html(
+			//Toggle between Save and Edit
+			text == "Save" ? "Edit" : "Save"
+		);
+	});
+});
