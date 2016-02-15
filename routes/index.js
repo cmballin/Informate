38e3f3eb689ad2a93b7491573a4892210de2ffa7
Daisy Aurora <daurora@ucsdedu>
2016-02-15 23:48:59 +0000

    update indeindex.js

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,21 @@ var data = require('../data.json');
 exports.view = function(req, res){
 	//console.log(data);
 	res.render('index', data);
+	var fulldesc = req.query.newcontent;
+	//self.teaser = "fjdksafjs";
+	this.teaser = fulldesc;
 
 	var newPost = {
-		"title": "My name is Bob",
-		"description": "I make burgers everyday",
-		"imageURL": "http://lorempixel.com/400/400/people"
+		"title": req.query.newtitle,
+		"teaser": this.teaser,
+		"description": req.query.newcontent,
+		"imageURL": "http://lorempixel.com/400/400/people",
+		"postnumber": req.query.postnumber,
+		"know": "0",
+		"notknow": "0"
 	}
+
+	console.log(newPost);
 
 	data["posts"].push(newPost);
 };
