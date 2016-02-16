82231372bb5982d651cdea446d78307ffd825503
Patrick Huang <patrickhuang94@gmail.com>
2016-02-16 08:29:05 +0000

    Teaser done

--- routes/addpost.js
+++ routes/addpost.js
@@ -10,14 +10,15 @@ exports.addnewpost = function(req, res){
 	//console.log(data);
 	res.render('index', data);
 	var fulldesc = req.query.newcontent;
-	//self.teaser = "fjdksafjs";
-	this.teaser = fulldesc;
+
+	//Grabs the first sentence as a teaser for posts
+	var teaser = fulldesc.split('.')[0];
 	this.postnumber = data.posts.length;
 
 	var newPost = {
 		"title": req.query.newtitle,
-		"teaser": this.teaser,
-		"description": req.query.newcontent,
+		"teaser": teaser,
+		"description": fulldesc,
 		"imageURL": "http://lorempixel.com/400/400/people",
 		"postnumber": this.postnumber,
 		"know": "0",
