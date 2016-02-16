4862a01405961d71aec180adc04c5700dbc908a2
Patrick Huang <patrickhuang94@gmail.com>
2016-02-16 01:04:10 +0000

    Deleted comment

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,5 @@ var data = require('../data.json');
 exports.view = function(req, res){
 	//console.log(data);
 	res.render('index', data);
-	/*
-	var fulldesc = req.query.newcontent;
-	this.teaser = fulldesc;
-	this.postnumber = data.posts.length;
 
-	var newPost = {
-		"title": req.query.newtitle,
-		"teaser": this.teaser,
-		"description": req.query.newcontent,
-		"imageURL": "http://lorempixel.com/400/400/people",
-		"postnumber": this.postnumber,
-		"know": "0",
-		"notknow": "0"
-	}
-
-	data["posts"].push(newPost);
-	*/
 };
